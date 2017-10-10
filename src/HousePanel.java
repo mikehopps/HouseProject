89a1f8fcd302a221e6296c6872bdc23f5ea22055
mikehopps <mhopps@gmail.com>
2017-10-10 11:55:05 -0400

    Added mouse listener, so that if you click on the panel, it prints the coordinates where you clicked.  It may help you out a bit.

--- src/HousePanel.java
+++ src/HousePanel.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 /**
  * Created by CHANGE THIS TO YOUR NAME on 10/2/17.
@@ -9,6 +11,35 @@ public class HousePanel extends JPanel{
 
     public HousePanel(int width, int height){
         setSize(width,height);
+
+
+        addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                System.out.println(e.getX() + ", " + e.getY());
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+
+            }
+        });
+
     }
 
     public void paintComponent(Graphics g) {
